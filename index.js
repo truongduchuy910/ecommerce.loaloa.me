0da6a1ef1cfb80517f1a6b03e2b66b7e0e0a9b09
huy <truongduchuy910@gmail.com>
2020-08-20 21:13:13 +0700

    OK

--- index.js
+++ index.js
@@ -31,15 +31,6 @@ ports.forEach(file => {
   }
 });
 
-// find and listen app
-let files = fs.readdirSync('./ports');
-const express = require('express');
-files.forEach(file => {
-  let app = express();
-  app.use(express.static(__dirname + '/ports/' + file));
-  app.listen(Number(file));
-});
-
 let { PasswordAuthStrategy } = require('@keystonejs/auth-password');
 let authStrategy = keystone.createAuthStrategy({
   type: PasswordAuthStrategy,
